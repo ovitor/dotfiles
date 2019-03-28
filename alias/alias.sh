9356bedd7b37c1609d1119408982901b6ff64eb1
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2019-03-28 15:05:21 -0300

    adding windows rdp alias

--- alias/alias.sh
+++ alias/alias.sh
@@ -6,3 +6,6 @@ alias tree="tree -C"
 alias t1="tree -L 1"
 alias pbpaste="xclip -selection clipboard -o"
 alias pbcopy="xclip -selection c"
+
+alias gcds="echo 'conectando a servidor: 200.17.33.66' && xfreerdp /u:'adproducao.ifce.edu.br\Administrator' /v:200.17.33.66"
+alias academico="echo 'conectando a servidor: 200.17.33.148' && xfreerdp /u:'Administrator' /v:200.17.33.148"
