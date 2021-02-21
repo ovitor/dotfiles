0d3f0e55e874a97930c39ebd695581def407864e
Vitor Carvalho <vcml@pm.me>
2021-02-21 19:37:13 -0300

    updating several stuff

--- alias/alias.sh
+++ alias/alias.sh
@@ -4,12 +4,14 @@ alias update='sudo pacman -Suy; trizen -Syyu'
 # SSH
 alias ssh="TERM=xterm-color ssh"
 
-# Simple tree
+# Simple comands
 alias ip="ip -c"
 alias tree="tree -C"
 alias t1="tree -L 1"
 alias pbpaste="xclip -selection clipboard -o"
 alias pbcopy="xclip -selection c"
+alias diff="diff --color=auto"
+alias grep="grep --color=auto"
 
 # other programs
 alias feh="feh -."
@@ -17,7 +19,6 @@ alias 3n="nnn"
 
 # code
 alias code-python="tmux new-session -s python \; send-keys 'vim .' C-m \; split-window -v -p 25 \; send-keys 'poetry shell' C-m \; split-window -h -p 30 \; send-keys 'poetry shell' C-m \; select-pane -t 0"
-alias hive="tmux new-session -s hive \; new-window \; new-window \; new-window \; new-window \; select-window -t 0"
 
 # record outputstream (needs configuration with pavucontrol)
 alias record="ffmpeg -f pulse -ac 2 -i default -acodec libmp3lame"
